f6652a4d090958733310c7beb6eecdc0be6c1fc3
deniskulakovskiy <deniskulakovskiy18@yandex.ru>
2021-01-26 11:23:11 +0300

    second

--- first.py
+++ first.py
@@ -1,1 +1,66 @@
-print("This is my first git repo")
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPen
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(500, 500)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(210, 200, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+
+
+class Example(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.run)
+        self.ok = False
+
+    def run(self):
+        self.ok = True
+
+    def paintEvent(self, event):
+        if self.ok:
+            self.qp = QPainter(self)
+            self.qp.begin(self)
+            self.draw()
+            self.qp.end()
+            self.update()
+
+    def draw(self):
+        self.qp.setPen(QPen(Qt.green, 8, Qt.SolidLine))
+        self.qp.drawEllipse(100, 100, 100, 100)
+        self.qp.setPen(QPen(Qt.yellow, 8, Qt.SolidLine))
+        self.qp.drawEllipse(200, 200, 100, 100)
+        self.qp.setPen(QPen(Qt.red, 8, Qt.SolidLine))
+        self.qp.drawEllipse(100, 200, 100, 100)
+
+
+app = QApplication(sys.argv)
+ex = Example()
+ex.show()
+app.exit(app.exec())
